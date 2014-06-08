374c600e07c5f01551ef9673d83aa4c18501b42e
Matt Blair <me@matthewblair.net>
2014-06-08 14:47:49 -0700

    add jenkins runner

---
+++ angular-via-yeoman/jenkins.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PATH="/usr/local/bin/:${PATH}"
+export NODE_PATH="/usr/local/lib/node_modules"
+/usr/local/bin/npm install
+/usr/local/bin/bower install
+./node_modules/.bin/grunt --no-color --stack --verbose jshint test build
